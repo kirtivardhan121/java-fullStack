4d1eb5afde583559166b087fb601737ccd22be4e
kirtivardhan121 <i.kirtivardhan@outlook.com>
2019-12-04 20:52:42 +0530

    Spring-boot

---
+++ springboot/src/main/java/com/ustglobal/springboot/repo/EmployeeRepo.java
@@ -0,0 +1,24 @@
+package com.ustglobal.springboot.repo;
+
+import java.util.Date;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ustglobal.springboot.dto.EmployeeBean;
+
+
+public interface EmployeeRepo extends JpaRepository<EmployeeBean, Integer> {
+
+	@Transactional
+	@Modifying
+	@Query("update EmployeeBean set name=:name,password=:password,gender=:gender,doj=:doj where id=:id")
+	public void update(@Param("name")String name,@Param("password") String password,@Param("gender") String gender,@Param("doj") Date doj,@Param("id") int id);
+	
+	public EmployeeBean findByName(String name);
+	
+	public void deleteByName(String name);
+}
